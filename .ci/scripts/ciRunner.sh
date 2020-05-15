2765f5e59d6f20fdd5849d58011ab74d816078c6
Wolf Utz <wpu@hotmail.de>
2020-05-15 14:48:44 +0200

    try fix travis #3

--- .ci/scripts/ciRunner.sh
+++ .ci/scripts/ciRunner.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-
-echo "${PUBLIC_TEST_KEY}"
+echo -n "${PUBLIC_TEST_KEY}" | wc -c
 
 # Function to write a .env file in Build/docker
 # This is read by docker-compose and vars defined here are
