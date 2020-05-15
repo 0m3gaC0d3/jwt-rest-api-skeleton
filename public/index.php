112f4e578d2e5b9fa9c56e0a9df5d6bc1357d2b7
Wolf Utz <wpu@hotmail.de>
2020-05-15 17:41:52 +0200

    try fix travis #20

--- public/index.php
+++ public/index.php
@@ -9,14 +9,5 @@ use OmegaCode\JwtSecuredApiCore\Core\Kernel\HttpKernel;
     require APP_ROOT_PATH . 'vendor/autoload.php';
     $envFile = $_ENV['APPLICATION_ENVIRONMENT'] === 'test' ? '.env.test' : '.env';
     (new \Symfony\Component\Dotenv\Dotenv())->loadEnv(APP_ROOT_PATH . $envFile);
-    // Create test keys if not present.
-    if ($_ENV['APPLICATION_ENVIRONMENT'] === 'test') {
-        if (!file_exists(APP_ROOT_PATH . $_ENV['PRIVATE_KEY_PATH'])) {
-            file_put_contents(APP_ROOT_PATH . $_ENV['PRIVATE_KEY_PATH'], getenv('PRIVATE_TEST_KEY'));
-        }
-        if (!file_exists(APP_ROOT_PATH . $_ENV['PUBLIC_KEY_PATH'])) {
-            file_put_contents(APP_ROOT_PATH . $_ENV['PUBLIC_KEY_PATH'], getenv('PUBLIC_TEST_KEY'));
-        }
-    }
     (new HttpKernel())->run();
 })();
