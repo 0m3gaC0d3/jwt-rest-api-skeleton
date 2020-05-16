c421973ebb599c246929b134787e799e1e01fe9d
Wolf Utz <wpu@hotmail.de>
2020-05-16 23:55:09 +0200

    Add unit tests and remove unnecessary envs
    Add ArrayUtility and StringUtility unit tests.
    Fix StringUtility::camelCaseToSnakeCase.
    Remove unnecessary envs in .ci/docker/docker-compose.yml.

--- tests/Api/JWTAuthTest.php
+++ tests/Api/JWTAuthTest.php
@@ -1,5 +1,29 @@
 <?php
 
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020 Wolf Utz<wpu@hotmail.de>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 declare(strict_types=1);
 
 namespace OmegaCode\JwtSecuredApiCore\Tests\Api;
@@ -21,7 +45,7 @@ class JWTAuthTest extends TestCase
      */
     public function accessTokenRouteWithOutSecretReturns401(): void
     {
-        $response = $this->client->request('POST', 'http://api/auth', ['http_errors' => false,]);
+        $response = $this->client->request('POST', 'http://api/auth', ['http_errors' => false]);
         $this->assertEquals(401, $response->getStatusCode());
     }
 
@@ -32,7 +56,7 @@ class JWTAuthTest extends TestCase
     {
         $response = $this->client->request('POST', 'http://api/auth', [
             'http_errors' => false,
-            'json' => ["clientId" => "wrong-secret"]
+            'json' => ['clientId' => 'wrong-secret'],
         ]);
         $this->assertEquals(401, $response->getStatusCode());
     }
@@ -44,7 +68,7 @@ class JWTAuthTest extends TestCase
     {
         $response = $this->client->request('POST', 'http://api/auth', [
             'http_errors' => false,
-            'json' => ["clientId" => "1234"]// @see res/test/api/clients.test.json
+            'json' => ['clientId' => '1234'], // @see res/test/api/clients.test.json
         ]);
         $this->assertEquals(201, $response->getStatusCode());
     }
@@ -59,7 +83,7 @@ class JWTAuthTest extends TestCase
             'headers' => [
                 'Authorization' => 'Bearer wrong-token',
                 'Accept' => 'application/json',
-            ]
+            ],
         ]);
         $this->assertFalse(json_decode((string) $response->getBody(), true)['success']);
     }
@@ -71,7 +95,7 @@ class JWTAuthTest extends TestCase
     {
         $response = $this->client->request('POST', 'http://api/auth', [
             'http_errors' => false,
-            'json' => ["clientId" => "1234"]// @see res/test/api/clients.test.json
+            'json' => ['clientId' => '1234'], // @see res/test/api/clients.test.json
         ]);
         $result = json_decode((string) $response->getBody(), true);
         $this->assertArrayHasKey('access_token', $result);
@@ -81,7 +105,7 @@ class JWTAuthTest extends TestCase
             'headers' => [
                 'Authorization' => 'Bearer ' . $token,
                 'Accept' => 'application/json',
-            ]
+            ],
         ]);
         $this->assertTrue(json_decode((string) $response->getBody(), true)['success']);
     }
