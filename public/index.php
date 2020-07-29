f166bb7aabeb839e1ff9343cd794e64471209c8a
Wolf Utz <wpu@hotmail.de>
2020-07-29 15:05:27 +0200

    Rename database env variables to minimize name collisions

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ use OmegaCode\JwtSecuredApiCore\Core\Kernel\HttpKernel;
 (function () {
     define('APP_ROOT_PATH', dirname(__DIR__, 1) . '/');
     require APP_ROOT_PATH . 'vendor/autoload.php';
-    $envFile = $_ENV['APP_ENV'] === 'test' ? '.env.test' : '.env';
+    $envFile = isset($_ENV['APP_ENV']) && $_ENV['APP_ENV'] === 'test'  ? '.env.test' : '.env';
     (new \Symfony\Component\Dotenv\Dotenv())->loadEnv(APP_ROOT_PATH . $envFile);
     (new HttpKernel())->run();
 })();
